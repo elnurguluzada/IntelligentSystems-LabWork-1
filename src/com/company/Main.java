71c34ac36bf7a78dfc2c52df41cfbce71a1ba954
elnurguluzada <kuliyevelnur@gmail.com>
2020-10-06 19:05:45 +0300

    doing perceptron  training for 7 elements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,34 @@ public class Main {
     public static void main(String[] args) {
 
 
+
+
         Perceptron perceptron = new Perceptron();
 
+        float[] weights = new float[2];
+        float bias;
+        float learningRate = (float) 0.2;
 
-        perceptron.calculateOutput();
+        double[] arr =  perceptron.calculateOutput(perceptron.initializeWeights(), perceptron.initializeBias());
+
+
+           while (arr[0] != 0){
+
+               weights[0]  = perceptron.updateWeight((float) arr[0], (int) arr[4], arr[1] , learningRate);
+               weights[1]  = perceptron.updateWeight((float) arr[2], (int) arr[4], arr[3] , learningRate);
+               bias =  perceptron.updateBias((float) arr[5] , (int) arr[4] ,learningRate);
+               arr = perceptron.calculateOutput(weights, bias);
+
+               System.out.println("Error = " + (int) arr[4]);
+               System.out.println("weight[0] = " + (int) arr[0]);
+
+
+           }
+
+
+
+
+
 
 
 
--- src/com/company/Perceptron.java
+++ src/com/company/Perceptron.java
@@ -6,13 +6,15 @@ public class Perceptron {
 
 
     float[] weights = new float[2];
-
     float learningRate = (float) 0.2;
     float bias ;
+    int sumOfDesiredOutput;
+    int sumOfReceviedOutput;
 
     int error;
 
     int[] desiredOutput ={1,1,1,1,-1,-1,-1};
+    int[] receviedOutput = new int[7];
     int output ;
 
     double[] apple1 = {0.21835,0.81884,1 };
@@ -35,12 +37,6 @@ public class Perceptron {
 
 
 
-
-
-
-
-
-
     int makeDecision( float v){
 
         if(v >= 0 ){
@@ -53,309 +49,275 @@ public class Perceptron {
     }
 
 
-    void calculateOutput(){
+
+    float updateWeight(float weight , int error , double input , float learningRate){
+
+
+            weight+= learningRate*error*input;
+
+
+
+        return weight;
+    }
+
+
+    float updateBias(float bias , int error  , float learningRate){
+
+
+            bias+= learningRate*error;
+
+
+        return bias;
+    }
+
+
+
+    float[] initializeWeights(){
+
+        for (int i = 0; i < weights.length; i++) {
+
+            weights[i] = (float) (Math.random() * (1 - (0)));
+//            System.out.println(" weights[" + i +"]  = " +weights[i]);
+        }
+
+
+        return weights;
+    }
+
+
+
+    float initializeBias(){
+
+        // initializing bias between 0 and 1
+        bias = (float) (Math.random() * (1 - (0)));
+//        System.out.println("Bias = " + bias);
+
+        return bias;
+    }
+
+
+
+    double[]  calculateOutput(float[] weights , float bias){
 
         float v;
 
+        double[] zero = {0};
 
         System.out.println("*************** APPLES ****************");
-        // apple 1
+            System.out.println();
+            // apple 1
 
-        // initializing weights between 0 and 1
-        for (int i = 0; i < weights.length; i++) {
-
-            weights[i] = (float) (Math.random() * (1 - (0)));
-            System.out.println(" weights[" + i +"] for apple 1 = " +weights[i]);
-        }
-
-        // initializing bias between 0 and 1
-        bias = (float) (Math.random() * (1 - (0)));
-        System.out.println("Bias = " + bias);
+            // initializing weights between 0 and 1
 
 
-        //calculating v
-        v = (float) (apple1[0]*weights[0] + apple1[1]*weights[1] + bias);
-        System.out.println("V for apple 1 = " + v);
+            //calculating v
+            v = (float) (apple1[0] * weights[0] + apple1[1] * weights[1] + bias);
+            System.out.println("V for apple 1 = " + v);
 
-        //calculating output
-        output = makeDecision(v);
-        System.out.println("y for apple 1 = " + output);
-
-        //calculating error
-        error = desiredOutput[0] - output;
-        System.out.println("error before training for apple 1  = " + error);
-
-        if(error != 0){
-
-            //updating weights and bias
-
-            for (int i = 0; i < 100; i++){
-                weights[0]+= learningRate*error*apple1[0];
-                weights[1] += learningRate*error*apple1[1];
-                bias+=learningRate*error;
+            //calculating output
+            output = makeDecision(v);
+            System.out.println("y for apple 1 = " + output);
+            receviedOutput[0] = output;
 
 
-                v = (float) (apple1[0]*weights[0] + apple1[1]*weights[1] + bias);
-                output=makeDecision(v);
-                error=desiredOutput[0]-output;
-
-            }
             System.out.println("error after training for apple 1  = " + error);
 
 
-        }
+            System.out.println("*******************************");
+            System.out.println();
 
 
-        System.out.println("*******************************");
+            // apple 4
 
 
-        // apple 4
+            v = (float) (apple4[0] * weights[0] + apple4[1] * weights[1] + bias);
+            System.out.println("V for apple 4 = " + v);
+            output = makeDecision(v);
+            System.out.println("y for apple 4 = " + output);
+            receviedOutput[1] = output;
 
 
-        for (int i = 0; i < weights.length; i++) {
-
-            weights[i] = (float) (Math.random() * (1 - (0)));
-            System.out.println(" weights[" + i +"] for apple 4 = " +weights[i]);
-        }
-
-        bias = (float) (Math.random() * (1 - (0)));
-        System.out.println("Bias = " + bias);
-
-
-        v = (float) (apple4[0]*weights[0] + apple4[1]*weights[1] + bias);
-        System.out.println("V for apple 4 = " + v);
-        output = makeDecision(v);
-        System.out.println("y for apple 4 = " + output);
-
-        error = desiredOutput[1] - output;
-        System.out.println("error before training for apple 4  = " + error);
-
-        if(error != 0){
-
-
-            for (int i = 0; i < 100; i++){
-                weights[0]+= learningRate*error*apple4[0];
-                weights[1] += learningRate*error*apple4[1];
-                bias+=learningRate*error;
-
-                v = (float) (apple4[0]*weights[0] + apple4[1]*weights[1] + bias);
-                output=makeDecision(v);
-                error=desiredOutput[1]-output;
-
-            }
             System.out.println("error after training for apple 4  = " + error);
 
 
-        }
+            System.out.println("*******************************");
+            System.out.println();
 
-        System.out.println("*******************************");
+            // apple 7
 
-        // apple 7
 
+            v = (float) (apple7[0] * weights[0] + apple7[1] * weights[1] + bias);
+            System.out.println("V for apple 7 = " + v);
+            output = makeDecision(v);
+            System.out.println("y for apple 7 = " + output);
 
-        for (int i = 0; i < weights.length; i++) {
 
-            weights[i] = (float) (Math.random() * (1 - (0)));
-            System.out.println(" weights[" + i +"] for apple 7 = " +weights[i]);
-        }
+            receviedOutput[2] = output;
 
-        bias = (float) (Math.random() * (1 - (0)));
-        System.out.println("Bias = " + bias);
 
+            System.out.println("*******************************");
+            System.out.println();
 
-        v = (float) (apple7[0]*weights[0] + apple7[1]*weights[1] + bias);
-        System.out.println("V for apple 7 = " + v);
-        output = makeDecision(v);
-        System.out.println("y for apple 7 = " + output);
 
-        error = desiredOutput[2] - output;
-        System.out.println("error before training for apple 7  = " + error);
+            // apple 3
 
-        if(error != 0){
 
+            //calculating v
+            v = (float) (apple3[0] * weights[0] + apple3[1] * weights[1] + bias);
+            System.out.println("V for apple 3 = " + v);
+            //calculating output
+            output = makeDecision(v);
+            System.out.println("y for apple 3 = " + output);
+            receviedOutput[3] = output;
 
-            for (int i = 0; i < 100; i++){
-                weights[0]+= learningRate*error*apple7[0];
-                weights[1] += learningRate*error*apple7[1];
-                bias+=learningRate*error;
 
-                v = (float) (apple7[0]*weights[0] + apple7[1]*weights[1] + bias);
-                output=makeDecision(v);
-                error=desiredOutput[2]-output;
+            System.out.println("*************** PEARS ****************");
+            System.out.println();
 
-            }
-            System.out.println("error after training for apple 7  = " + error);
+            // For pear 2
 
 
-        }
+            v = (float) (pear2[0] * weights[0] + pear2[1] * weights[1] + bias);
+            System.out.println("V = " + v);
+            output = makeDecision(v);
+            System.out.println("Output = " + output);
+            receviedOutput[4] = output;
 
 
-        System.out.println("*******************************");
+            System.out.println("*******************************");
 
 
-        // apple 3
+            // For pear 4
 
-        // initializing weights between 0 and 1
-        for (int i = 0; i < weights.length; i++) {
 
-            weights[i] = (float) (Math.random() * (1 - (0)));
-            System.out.println(" weights[" + i +"] for apple 3 = " +weights[i]);
-        }
+            v = (float) (pear4[0] * weights[0] + pear4[1] * weights[1] + bias);
 
-        // initializing bias between 0 and 1
-        bias = (float) (Math.random() * (1 - (0)));
-        System.out.println("Bias = " + bias);
+            System.out.println("V = " + v);
 
+            output = makeDecision(v);
+            System.out.println("Output for pear 4 = " + output);
+            receviedOutput[5] = output;
 
-        //calculating v
-        v = (float) (apple3[0]*weights[0] + apple3[1]*weights[1] + bias);
-        System.out.println("V for apple 3 = " + v);
 
-        //calculating output
-        output = makeDecision(v);
-        System.out.println("y for apple 3 = " + output);
+            System.out.println("*******************************");
 
-        //calculating error
-        error = desiredOutput[3] - output;
-        System.out.println("error before training for apple 3  = " + error);
+            // For pear 1
 
-        if(error != 0){
+            v = (float) (pear1[0] * weights[0] + pear1[1] * weights[1] + bias);
+            System.out.println("V = " + v);
 
-            //updating weights and bias
+            output = makeDecision(v);
+            System.out.println("Output for pear 1 = " + output);
+            receviedOutput[6] = output;
 
-            for (int i = 0; i < 100; i++){
-                weights[0]+= learningRate*error*apple3[0];
-                weights[1] += learningRate*error*apple3[1];
-                bias+=learningRate*error;
 
-
-                v = (float) (apple3[0]*weights[0] + apple3[1]*weights[1] + bias);
-                output=makeDecision(v);
-                error=desiredOutput[3]-output;
-
-            }
-            System.out.println("error after training for apple 3  = " + error);
-
-
-        }
-
-
-        System.out.println("*************** PEARS ****************");
-
-        // For pear 2
-
-        for (int i = 0; i < weights.length; i++) {
-
-            weights[i] = (float) (Math.random() * (1 - (0)));
-            System.out.println(" weights[" + i +"] for pear 2 = " +weights[i]);
-        }
-
-        v = (float) (pear2[0]*weights[0] + pear2[1]*weights[1] + bias);
-
-        System.out.println("V = " + v);
-
-         output = makeDecision(v);
-        System.out.println("Output = " + output);
-
-         error = desiredOutput[4] - output;
-        System.out.println("Error = " + error);
-
-         if(error != 0) {
-
-             System.out.println("Error (before training) = " + error);
-
-             for (int i = 0; i < 100; i++) {
-
-                 weights[0] += learningRate * error * pear2[0];
-                 weights[1] += learningRate * error * pear2[1];
-                 bias+= learningRate*error;
-
-                 v = (float) (pear2[0]*weights[0] + pear2[1]*weights[1] + bias);
-                 output = makeDecision(v);
-                 error = desiredOutput[4] - output;
-             }
-
-             System.out.println("Error (after training) = " + error);
-
-         }
-
-        System.out.println("*******************************");
-
-
-        // For pear 4
-
-        for (int i = 0; i < weights.length; i++) {
-
-            weights[i] = (float) (Math.random() * (1 - (0)));
-            System.out.println(" weights[" + i +"] for pear 4 = " +weights[i]);
-        }
-
-        v = (float) (pear4[0]*weights[0] + pear4[1]*weights[1] + bias);
-
-        System.out.println("V = " + v);
-
-        output = makeDecision(v);
-        System.out.println("Output for pear 4 = " + output);
-
-        error = desiredOutput[5] - output;
-        System.out.println("Error for pear 4 = " + error);
-
-        if(error != 0) {
-
-            System.out.println("Error (before training) for pear 4 = " + error);
-
-            for (int i = 0; i < 100; i++) {
-
-                weights[0] += learningRate * error * pear4[0];
-                weights[1] += learningRate * error * pear4[1];
-                bias+= learningRate*error;
-
-                v = (float) (pear4[0]*weights[0] + pear4[1]*weights[1] + bias);
-                output = makeDecision(v);
-                error = desiredOutput[5] - output;
+            System.out.println();
+            System.out.print("desiredOutput[ ");
+            for (int dO : desiredOutput) {
+                sumOfDesiredOutput+=dO;
+                System.out.print(dO + ",");
             }
 
-            System.out.println("Error (after training)  for pear 4 = " + error);
-
-        }
+            System.out.println("]");
 
 
-        System.out.println("*******************************");
-
-        // For pear 1
-
-        for (int i = 0; i < weights.length; i++) {
-
-            weights[i] = (float) (Math.random() * (1 - (0)));
-            System.out.println(" weights[" + i +"] for pear 1 = " +weights[i]);
-        }
-
-        v = (float) (pear1[0]*weights[0] + pear1[1]*weights[1] + bias);
-
-        System.out.println("V = " + v);
-
-        output = makeDecision(v);
-        System.out.println("Output for pear 1 = " + output);
-
-        error = desiredOutput[6] - output;
-        System.out.println("Error (before training) for pear 4 = " + error);
-
-        if(error != 0) {
-
-
-
-            for (int i = 0; i < 100; i++) {
-
-                weights[0] += learningRate * error * pear1[0];
-                weights[1] += learningRate * error * pear1[1];
-                bias+= learningRate*error;
-
-                v = (float) (pear1[0]*weights[0] + pear1[1]*weights[1] + bias);
-                output = makeDecision(v);
-                error = desiredOutput[6] - output;
+            System.out.print("receviedOutput[ ");
+            for (int ro : receviedOutput) {
+                sumOfReceviedOutput+=ro;
+                System.out.print(ro + ",");
             }
 
-            System.out.println("Error (after training)  for pear 1 = " + error);
+            System.out.println("]");
+
+            if((desiredOutput[0] - receviedOutput[0]) != 0){
+                double[] arr = {weights[0],apple1[0] ,weights[1] ,  apple1[1] ,desiredOutput[0] - receviedOutput[0] , bias };
+                return arr;
+            } else if((desiredOutput[1] - receviedOutput[1]) != 0){
+                double[] arr = {weights[0], apple4[0] , weights[1],apple4[1] ,desiredOutput[1] - receviedOutput[1], bias };
+                return arr;
+            } else if((desiredOutput[2] - receviedOutput[2]) != 0){
+                double[] arr = {weights[0], apple7[0] , weights[1], apple7[1] ,desiredOutput[2] - receviedOutput[2] , bias};
+                return arr;
+            } else if((desiredOutput[3] - receviedOutput[3]) != 0){
+                double[] arr = {weights[0],apple3[0] ,weights[1], apple3[1] ,desiredOutput[3] - receviedOutput[3], bias };
+                return arr;
+            } else if((desiredOutput[4] - receviedOutput[4]) != 0){
+                double[] arr = {weights[0],pear2[0] , weights[1],pear2[1] ,desiredOutput[4] - receviedOutput[4] , bias};
+                return arr;
+            }else if((desiredOutput[5] - receviedOutput[5]) != 0){
+                double[] arr = {weights[0],pear4[0] ,weights[1], pear4[1] ,desiredOutput[5] - receviedOutput[5], bias };
+                return arr;
+            }else if((desiredOutput[6] - receviedOutput[6]) != 0){
+                double[] arr = {weights[0], pear1[0] ,weights[1], pear1[1] ,desiredOutput[6] - receviedOutput[6] , bias};
+                return arr;
+            } else {
+                return zero ;
+            }
+
+
+
+
+
+//        System.out.println("*************** Other fruits ****************");
+//        System.out.println();
+//
+//
+//
+//        System.out.println();
+//        System.out.println("Apple 2 ");
+//        System.out.println();
+//
+//
+//
+//        v = (float) (weights[0]*apple2[0] + weights[1]*apple2[1] + bias);
+//        output = makeDecision(v);
+//        System.out.println(" output for apple 2  = " + output);
+//
+//        System.out.println();
+//        System.out.println("Apple 4 ");
+//        System.out.println();
+//
+//
+//
+//        v = (float) (weights[0]*apple4[0] + weights[1]*apple4[1] + bias);
+//        output = makeDecision(v);
+//        System.out.println(" output for apple 4  = " + output);
+//
+//
+//        System.out.println();
+//        System.out.println("Apple 7");
+//        System.out.println();
+//
+//
+//
+//        v = (float) (weights[0]*apple7[0] + weights[1]*apple7[1] + bias);
+//        output = makeDecision(v);
+//        System.out.println(" output for apple 7  = " + output);
+//
+//        System.out.println();
+//        System.out.println("Apple 3 ");
+//        System.out.println();
+//
+//
+//
+//        v = (float) (weights[0]*apple3[0] + weights[1]*apple3[1] + bias);
+//        output = makeDecision(v);
+//
+//        System.out.println(" output for apple 3  = " + output);
+//
+//
+//
+//        // pear
+//
+//
+//        System.out.println();
+//        System.out.println("Pear 3 ");
+//        System.out.println();
+//
+//        v = (float) (weights[0]*pear3[0] + weights[1]*pear3[1] + bias);
+//        output = makeDecision(v);
+//
+//        System.out.println(" output for pear 3  = " + output);
 
         }
 
@@ -366,4 +328,5 @@ public class Perceptron {
 
 
 
-}
+
+
